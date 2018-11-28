7f117e9c0366abf88c2bdab4271c4fe02e2353d7
Nadir Nibras <32527014+nadir2115@users.noreply.github.com>
2018-11-27 23:31:47 -0800

    Added data preparation to R script
    Added data preparation to R script
    Also got rid of casual and registered users

--- Bike regression script.R
+++ Bike regression script.R
@@ -13,7 +13,8 @@ library(readr)
 library(datasets)
 library(ggplot2)
 library(ggExtra)
-
+library(rstudioapi)
+library(corrplot)
 
 # set directory to R script folder
 current_path <- getActiveDocumentContext()$path 
@@ -21,26 +22,80 @@ setwd(dirname(current_path ))
 
 # load datasets
 rawData<- read_csv("hour.csv")
-processedData <- read_csv("bikehour.csv")
+
+
+# Pre-processing data -----------------------------------------------------
+
+
+processedData<-rawData
+processedData$weeknum<-week(processedData$dteday)
+processedData$dteday<- NULL
+processedData<-processedData[,c(1,17,2:16)]
+
+# one hot encoding with loop
+for(i in unique(processedData$season)) {
+  processedData[[paste0("season_",i)]] <- ifelse(processedData$season==i,1,0)
+}
+for(i in unique(processedData$weekday)) {
+  processedData[[paste0("weekday_",i)]] <- ifelse(processedData$weekday==i,1,0)
+}
+for(i in unique(processedData$weathersit)) {
+  processedData[[paste0("weathersit_",i)]] <- ifelse(processedData$weathersit==i,1,0)
+}
+for(i in unique(processedData$mnth)) {
+  processedData[[paste0("mnth_",i)]] <- ifelse(processedData$mnth==i,1,0)
+}
+for(i in unique(processedData$hr)) {
+  processedData[[paste0("hour_",i)]] <- ifelse(processedData$hr==i,1,0)
+}
+for(i in unique(processedData$weekday)) {
+  processedData[[paste0("weekday_",i)]] <- ifelse(processedData$weekday==i,1,0)
+}
+for(i in unique(processedData$weeknum)) {
+  processedData[[paste0("weeknum_",i)]] <- ifelse(processedData$weeknum==i,1,0)
+}
+
+
+# Fix cyclic variables
+processedData$weeknum<-pmin(abs(3-processedData$weeknum),56-processedData$weeknum);  
+processedData$mnth<-pmin(abs(1-processedData$mnth),13-processedData$mnth);  
+processedData$hr<-pmin(abs(4-processedData$hr),28-processedData$hr);  
+
+# Min-max normalization
+processedData$weeknum<-processedData$weeknum/max(processedData$weeknum)
+processedData$mnth<-processedData$mnth/max(processedData$mnth)
+processedData$hr<- processedData$hr/max(processedData$hr)
+
+# move count to end
+processedData<- processedData[c(1:16, 18:121,17)]
+# remove excessive columns
+processedData<- processedData[-c(1,3,8,10,15,16)]
+
+
+
+
+# Exploratory data analysis -----------------------------------------------
+
 
 # plot  atemp
-p<-ggplot(processedData,aes(x=atemp,y=count))+
+p<-ggplot(processedData,aes(x=atemp,y=cnt))+
   geom_point(alpha=0.07, color='green')+
   labs(x='Adjusted Temperature', y= 'Hourly Usage Count')+
   geom_smooth(method='auto')+
   geom_smooth(method='lm',color= 'red')
 ggMarginal(p, type = "histogram", fill="transparent", margins=c("x"))
 
+
 # plot temp
-p<-ggplot(rawData,aes(x=temp,y=count))+
+p<-ggplot(rawData,aes(x=temp,y=cnt))+
   geom_point(alpha=0.07,color='orange')+
-  labs(x='Temperature', y= 'Hourly Usage Count')+
+  labs(x='Temperature', y= 'Hourly Usage cnt')+
   geom_smooth(method='auto')+
   geom_smooth(method='lm',color= 'red')
 ggMarginal(p, type = "histogram", fill="transparent", margins=c("x"))
 
 # plot humidity
-p<-ggplot(processedData,aes(x=hum,y=count))+
+p<-ggplot(processedData,aes(x=hum,y=cnt))+
   geom_point(alpha=0.07, color='violet')+
   labs(x='Humidity', y= 'Hourly Usage Count')+
   geom_smooth(method='auto')+
@@ -55,57 +110,55 @@ p<-ggplot(rawData,aes(x=weathersit,y=cnt))+
   geom_smooth(method='loess')
 ggMarginal(p, type = "histogram", fill="transparent", margins=c("x"))
 
-
 # plot wind-speed
-p<-ggplot(rawData,aes(x=windspeed,y=count))+
+p<-ggplot(rawData,aes(x=windspeed,y=cnt))+
   geom_point(alpha=0.07, color='green')+
   labs(x='Wind speed', y= 'Hourly Usage Count')+
   geom_smooth(method='auto')+
   geom_smooth(method='lm',color= 'red')
 ggMarginal(p, type = "histogram", fill="transparent", margins=c("x"))
 
-
-library(corrplot)
+# correlation plot
 cont_data<-rawData[c(11:17)]
 o=corrplot(cor(cont_data),method='number') # this method can be changed try using method='circle'
 
 # plot 24 hours
-ggplot(rawData,aes(x=hr,y=count))+
+ggplot(rawData,aes(x=hr,y=cnt))+
   geom_count(color= 'yellow1')+
   geom_point(alpha=0.05, color= 'coral')+
   labs(x='Military time', y= 'Usage count')+
   geom_smooth()
 
-# plot hours from 4am
-ggplot(processedData,aes(x=hours_from_4am,y=count))+
+# plot hours from 4am using processed Data
+ggplot(processedData,aes(x=hr,y=cnt))+
   geom_count(color='yellow')+
   geom_point(alpha=0.07, color= 'pink')+
   labs(x='Hours from 4am', y= 'Hourly Usage Count')+
-  geom_smooth(method='lm')+
+  geom_smooth(method='lm', color='red')+
   geom_smooth(method='loess', color ='blue')
 
 # Plot Months
-ggplot(rawData,aes(x= mnth,y=count))+
+ggplot(rawData,aes(x= mnth,y=cnt))+
   geom_count(color= 'yellow1')+
   geom_point(alpha=0.05, color= 'green')+
   labs(x='Month number', y= 'Hourly Usage Count')+
   geom_smooth()
 
 # plot months from January
-ggplot(processedData,aes(x=months_from_Jan,y=count))+
+ggplot(processedData,aes(x=mnth,y=cnt))+
   geom_count(color='yellow')+
   geom_point(alpha=0.07, color= 'pink')+
   labs(x='Months from January', y= 'Usage count')+
   geom_smooth(method='lm', color ='black')+
   geom_smooth(method='loess', color ='blue')
 
-# Boxplots
+# Boxplots -plot year vs count
 rawData$yr<-factor(rawData$yr)
-
-# plot year vs count
-ggplot(data=rawData,aes(x=time,y=cnt,  fill=yr ))+  
+ggplot(data=rawData,aes(x=yr,y=cnt,  fill=yr ))+  
   geom_boxplot(outlier.colour="Black",  outlier.size=1, notch=FALSE)+
   labs(x='Year', y= 'Hourly Usage Count')
 
-scatter.smooth(rawData$casual, rawData$registered)
+
+# MLE
+
 
