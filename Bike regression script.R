0c0fc142e7e3e45d9118d456d7b21125174ea6c4
Nadir Nibras <32527014+nadir2115@users.noreply.github.com>
2018-12-01 04:53:12 -0800

    Small changes to comments

--- Bike regression script.R
+++ Bike regression script.R
@@ -77,89 +77,89 @@ processedData<- processedData[c(1:16, 18:121,17)]
 processedData<- processedData[-c(1,3,8,10,15,16)]
 
 
-# # Exploratory data analysis -----------------------------------------------
-# 
-# 
-# # plot  atemp
-# p<-ggplot(processedData,aes(x=atemp,y=cnt))+
-#   geom_point(alpha=0.07, color='green')+
-#   labs(x='Adjusted Temperature', y= 'Hourly Usage Count')+
-#   geom_smooth(method='auto')+
-#   geom_smooth(method='lm',color= 'red')
-# ggMarginal(p, type = "histogram", fill="transparent", margins=c("x"))
-# 
-# 
-# # plot temp
-# p<-ggplot(rawData,aes(x=temp,y=cnt))+
-#   geom_point(alpha=0.07,color='orange')+
-#   labs(x='Temperature', y= 'Hourly Usage cnt')+
-#   geom_smooth(method='auto')+
-#   geom_smooth(method='lm',color= 'red')
-# ggMarginal(p, type = "histogram", fill="transparent", margins=c("x"))
-# 
-# # plot humidity
-# p<-ggplot(processedData,aes(x=hum,y=cnt))+
-#   geom_point(alpha=0.07, color='violet')+
-#   labs(x='Humidity', y= 'Hourly Usage Count')+
-#   geom_smooth(method='auto')+
-#   geom_smooth(method='lm',color= 'red')
-# ggMarginal(p, type = "histogram", fill="transparent", margins=c("x"))
-# 
-# # plot weather situation
-# p<-ggplot(rawData,aes(x=weathersit,y=cnt))+
-#   geom_count(color='yellow')+
-#   geom_point(alpha=0.07, color='green')+
-#   labs(x='Weather situation', y= 'Hourly Usage Count')+
-#   geom_smooth(method='loess')
-# ggMarginal(p, type = "histogram", fill="transparent", margins=c("x"))
-# 
-# # plot wind-speed
-# p<-ggplot(rawData,aes(x=windspeed,y=cnt))+
-#   geom_point(alpha=0.07, color='green')+
-#   labs(x='Wind speed', y= 'Hourly Usage Count')+
-#   geom_smooth(method='auto')+
-#   geom_smooth(method='lm',color= 'red')
-# ggMarginal(p, type = "histogram", fill="transparent", margins=c("x"))
-# 
-# # correlation plot
-# cont_data<-rawData[c(11:17)]
-# o=corrplot(cor(cont_data),method='number') # this method can be changed try using method='circle'
-# 
-# # plot 24 hours
-# ggplot(rawData,aes(x=hr,y=cnt))+
-#   geom_count(color= 'yellow1')+
-#   geom_point(alpha=0.05, color= 'coral')+
-#   labs(x='Military time', y= 'Usage count')+
-#   geom_smooth()
-# 
-# # plot hours from 4am using processed Data
-# ggplot(processedData,aes(x=hr,y=cnt))+
-#   geom_count(color='yellow')+
-#   geom_point(alpha=0.07, color= 'pink')+
-#   labs(x='Hours from 4am', y= 'Hourly Usage Count')+
-#   geom_smooth(method='lm', color='red')+
-#   geom_smooth(method='loess', color ='blue')
-# 
-# # Plot Months
-# ggplot(rawData,aes(x= mnth,y=cnt))+
-#   geom_count(color= 'yellow1')+
-#   geom_point(alpha=0.05, color= 'green')+
-#   labs(x='Month number', y= 'Hourly Usage Count')+
-#   geom_smooth()
-# 
-# # plot months from January
-# ggplot(processedData,aes(x=mnth,y=cnt))+
-#   geom_count(color='yellow')+
-#   geom_point(alpha=0.07, color= 'pink')+
-#   labs(x='Months from January', y= 'Usage count')+
-#   geom_smooth(method='lm', color ='black')+
-#   geom_smooth(method='loess', color ='blue')
-# 
-# # Boxplots -plot year vs count
-# rawData$yr<-factor(rawData$yr)
-# ggplot(data=rawData,aes(x=yr,y=cnt,  fill=yr ))+  
-#   geom_boxplot(outlier.colour="Black",  outlier.size=1, notch=FALSE)+
-#   labs(x='Year', y= 'Hourly Usage Count')
+# Exploratory data analysis -----------------------------------------------
+
+
+# plot  atemp
+p<-ggplot(processedData,aes(x=atemp,y=cnt))+
+  geom_point(alpha=0.07, color='green')+
+  labs(x='Adjusted Temperature', y= 'Hourly Usage Count')+
+  geom_smooth(method='auto')+
+  geom_smooth(method='lm',color= 'red')
+ggMarginal(p, type = "histogram", fill="transparent", margins=c("x"))
+
+
+# plot temp
+p<-ggplot(rawData,aes(x=temp,y=cnt))+
+  geom_point(alpha=0.07,color='orange')+
+  labs(x='Temperature', y= 'Hourly Usage cnt')+
+  geom_smooth(method='auto')+
+  geom_smooth(method='lm',color= 'red')
+ggMarginal(p, type = "histogram", fill="transparent", margins=c("x"))
+
+# plot humidity
+p<-ggplot(processedData,aes(x=hum,y=cnt))+
+  geom_point(alpha=0.07, color='violet')+
+  labs(x='Humidity', y= 'Hourly Usage Count')+
+  geom_smooth(method='auto')+
+  geom_smooth(method='lm',color= 'red')
+ggMarginal(p, type = "histogram", fill="transparent", margins=c("x"))
+
+# plot weather situation
+p<-ggplot(rawData,aes(x=weathersit,y=cnt))+
+  geom_count(color='yellow')+
+  geom_point(alpha=0.07, color='green')+
+  labs(x='Weather situation', y= 'Hourly Usage Count')+
+  geom_smooth(method='loess')
+ggMarginal(p, type = "histogram", fill="transparent", margins=c("x"))
+
+# plot wind-speed
+p<-ggplot(rawData,aes(x=windspeed,y=cnt))+
+  geom_point(alpha=0.07, color='green')+
+  labs(x='Wind speed', y= 'Hourly Usage Count')+
+  geom_smooth(method='auto')+
+  geom_smooth(method='lm',color= 'red')
+ggMarginal(p, type = "histogram", fill="transparent", margins=c("x"))
+
+# correlation plot
+cont_data<-rawData[c(11:17)]
+o=corrplot(cor(cont_data),method='number') # this method can be changed try using method='circle'
+
+# plot 24 hours
+ggplot(rawData,aes(x=hr,y=cnt))+
+  geom_count(color= 'yellow1')+
+  geom_point(alpha=0.05, color= 'coral')+
+  labs(x='Military time', y= 'Usage count')+
+  geom_smooth()
+
+# plot hours from 4am using processed Data
+ggplot(processedData,aes(x=hr,y=cnt))+
+  geom_count(color='yellow')+
+  geom_point(alpha=0.07, color= 'pink')+
+  labs(x='Hours from 4am', y= 'Hourly Usage Count')+
+  geom_smooth(method='lm', color='red')+
+  geom_smooth(method='loess', color ='blue')
+
+# Plot Months
+ggplot(rawData,aes(x= mnth,y=cnt))+
+  geom_count(color= 'yellow1')+
+  geom_point(alpha=0.05, color= 'green')+
+  labs(x='Month number', y= 'Hourly Usage Count')+
+  geom_smooth()
+
+# plot months from January
+ggplot(processedData,aes(x=mnth,y=cnt))+
+  geom_count(color='yellow')+
+  geom_point(alpha=0.07, color= 'pink')+
+  labs(x='Months from January', y= 'Usage count')+
+  geom_smooth(method='lm', color ='black')+
+  geom_smooth(method='loess', color ='blue')
+
+# Boxplots -plot year vs count
+rawData$yr<-factor(rawData$yr)
+ggplot(data=rawData,aes(x=yr,y=cnt,  fill=yr ))+
+  geom_boxplot(outlier.colour="Black",  outlier.size=1, notch=FALSE)+
+  labs(x='Year', y= 'Hourly Usage Count')
 
 # --------------------------------------------------------------------------
 
